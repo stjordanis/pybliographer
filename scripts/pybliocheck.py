stjordanis/pybliographer

commit c01060d350db5242adfb3b1e03765d6af755b29e

l10n: Add missing plural handling
https://wiki.gnome.org/TranslationProject/DevGuidelines/Plurals

https://bugzilla.gnome.org/show_bug.cgi?id=705246

scripts/pybliocheck.py
@@ -22,6 +22,8 @@
 
 import os, sys, string
 from Pyblio import Config, Exceptions
+from gettext import gettext as _
+from gettext import ngettext as _n
 
 import locale
 charset = locale.getlocale () [1] or 'ascii'
@@ -57,7 +59,10 @@ for dir in sys.argv [2:]:
         # try to open the database
         try:
             b = bibopen (f)
-            print (_("file `%s' is ok [%d entries]") % (f, len (b))).encode (charset)
+            print (_n("file “%s” is ok [%d entry]", 
+                      "file “%s” is ok [%d entries]", 
+                      len(b)) 
+                   % (f, len (b))).encode (charset)
         except (Exceptions.ParserError, KeyError), err:
             broken.append (str (err))
 

Pyblio/GnomeUI/Document.py
@@ -26,6 +26,7 @@
 from gi.repository import GObject
 
 from gettext import gettext as _
+from gettext import ngettext as _n 
 from gi.repository import Gtk, Gdk, GdkPixbuf
 
 from Pyblio.GnomeUI import Editor, Entry, FileSelector, Format
@@ -441,12 +442,10 @@ class Document (Connector.Publisher):
         
         if li == ld:
             if   ld == 0: num = _("[no entry]")
-            elif ld == 1: num = _("[1 entry]")
-            else:         num = _("[%d entries]")    %  ld
+            else:         num = _n("[%d entry]", "[%d entries]", ld) % ld
         else:
             if   ld == 0: num = _("[no entry]")
-            elif ld == 1: num = _("[%d/1 entry]")    % li
-            else:         num = _("[%d/%d entries]") % (li, ld)
+            else:         num = _n("[%d/%d entry]", "[%d/%d entries]", ld) % (li, ld)
 
         text = text + ' ' + num
         
@@ -954,7 +953,9 @@ class Document (Connector.Publisher):
         if l == 0: return
 
         if l > 5:
-            if not Utils.Callback (_("Really edit %d entries?")
+            if not Utils.Callback (_n("Really edit %d entry?", 
+                                      "Really edit %d entries?",
+                                      l)
                                    % l, parent = self.w).answer ():
                 return
 
@@ -1001,7 +1002,7 @@ class Document (Connector.Publisher):
         offset = self.index.get_item_position (entries [-1])
 
         if l > 1:
-            question = _("Remove all the %d entries?") % len (entries)
+            question = _n("Remove %d entry?", "Remove all %d entries?", l) % l
         else:
             question = _("Remove entry `%s'?") % entries [0].key.key
             

commit 4202149c7b475bdf6454449f5193f786bd133583

scripts: Fix regression when using plurals on pybliocheck
Fix encoding issue introduced in commit c01060d35.
It takes advantage of unicode, but strings are weird in
Python 2.

This also fixes make distcheck

scripts/pybliocheck.py
@@ -1,24 +1,25 @@
 # -*- python -*-
+# -*- coding: utf-8 -*-
 # This file is part of pybliographer
-# 
+#
 # Copyright (C) 1998-2004 Frederic GOBRY
 # Email : gobry@pybliographer.org
-# 	   
+#
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2 
+# as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-#   
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details. 
-# 
+# GNU General Public License for more details.
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-# 
-# 
+#
+#
 
 import os, sys, string
 from Pyblio import Config, Exceptions
@@ -61,9 +62,9 @@ for dir in sys.argv [2:]:
         # try to open the database
         try:
             b = bibopen (f)
-            print (_n("file “%s” is ok [%d entry]", 
-                      "file “%s” is ok [%d entries]", 
-                      len(b)) 
+            print (_n(u"file “%s” is ok [%d entry]",
+                      u"file “%s” is ok [%d entries]",
+                      len(b))
                    % (f, len (b))).encode (charset)
         except (Exceptions.ParserError, KeyError), err:
             broken.append (str (err))