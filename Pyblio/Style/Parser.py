9ea33470a297052dbe73a1c765747280c50696a3
Germán Poo-Caamaño <gpoo@gnome.org>
2018-03-28 12:02:38 -0300

    style: pyflakes

--- Pyblio/Style/Parser.py
+++ Pyblio/Style/Parser.py
@@ -1,33 +1,32 @@
 # -*- coding: utf-8 -*-
 # This file is part of pybliographer
-# 
+#
 # Copyright (C) 2018 Germán Poo-Caamaño <gpoo@gnome.org>
 # Copyright (C) 1998-2004 Frederic GOBRY <gobry@pybliographer.org>
-# 	   
+#
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2 
+# as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-#   
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details. 
-# 
+# GNU General Public License for more details.
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-# 
-# 
+#
 
 ''' Parser for XML definitions of the bibliographic output '''
 
-from Pyblio import Open, Autoload
+import string
+import re
 
 from xml import sax
-from xml.sax.saxutils import escape, quoteattr
 
-import string, re
+from Pyblio import Open, Autoload
 
 _map = string.maketrans ('\t', ' ')
 _cpt = re.compile ('\s+')
@@ -35,6 +34,7 @@ _rnl = re.compile ('\n\s*\n+')
 _dnl = re.compile ('\n')
 
 _lines = re.compile ('\d+-+\d+')
+
 
 class BibStyle:
     def __init__ (self):
