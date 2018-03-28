90d8ad71b75b27754e71f1b242454729571895c4
Germán Poo-Caamaño <gpoo@gnome.org>
2018-03-28 11:56:53 -0300

    output: pyflakes

--- Pyblio/Output/textnum.py
+++ Pyblio/Output/textnum.py
@@ -1,32 +1,32 @@
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 # This file is part of pybliographer
-# 
-# Copyright (C) 1998-2004 Frederic GOBRY
-# Email : gobry@pybliographer.org
-# 	   
+#
+# Copyright (C) 2018 Germán Poo-Caamaño <gpoo@gnome.org>
+# Copyright (C) 1998-2004 Frederic GOBRY <gobry@pybliographer.org>
+#
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2 
+# as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-#   
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details. 
-# 
+# GNU General Public License for more details.
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-# 
-# 
-# Modified version of text.py. It is used for abbrvnum.xml style.
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
+#
+# Modified version of text.py. It is used for abbrvau.xml style.
 # Created by Zoltán Kóta.
 
 """ This is a module that codes bibliographies into simple text output """
 
-import sys, string
+import string
 
 from Pyblio import Autoload, Utils, Formatter
+
 
 class TextFormat (Formatter.Formatter):
 
